4b38065895ddad01eb518f3863f87ac081ffa6ff
Jeff Martin <jeffmartin@gmail.com>
2018-06-05 21:19:27 -0400

    initial commit

---
+++ lib/plug_instrumenter.ex
@@ -0,0 +1,18 @@
+defmodule PlugInstrumenter do
+  @moduledoc """
+  Documentation for PlugInstrumenter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugInstrumenter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_instrumenter_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugInstrumenterTest do
+  use ExUnit.Case
+  doctest PlugInstrumenter
+
+  test "greets the world" do
+    assert PlugInstrumenter.hello() == :world
+  end
+end
