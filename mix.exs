58d77fc933bb53de9b41fb9824849c2e55a743da
Jeff Martin <jeff@teacherspayteachers.com>
2018-06-25 15:45:07 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugInstrumenter.MixProject do
   def project do
     [
       app: :plug_instrumenter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
